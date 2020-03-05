85f726780806e3c5d468968c0c4254ef95f4481e
Henry Sloan <henryksloan@gmail.com>
2020-03-04 21:28:09 -0500

    Allow repeat mirroring

--- include/mirror.h
+++ include/mirror.h
@@ -4,11 +4,12 @@
 
 class Mirror {
  public:
-    Mirror(uint16_t start, uint16_t end, uint16_t original_start)
-        : start(start), end(end), original_start(original_start) {};
+    Mirror(uint16_t start, uint16_t end, uint16_t original_start, uint16_t original_end)
+        : start(start), end(end),
+          original_start(original_start), original_end(original_end) {};
     bool contains(uint16_t addr);
     uint16_t map(uint16_t addr);
 
  protected:
-    uint16_t start, end, original_start;
+    uint16_t start, end, original_start, original_end;
 };
