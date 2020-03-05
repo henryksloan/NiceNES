2e44e99e4503a47d667f2484d73cef3a533b4ce0
Henry Sloan <henryksloan@gmail.com>
2020-03-04 21:18:54 -0500

    Consolidate mirror implementations to one class

--- include/mirror.h
+++ include/mirror.h
@@ -6,8 +6,8 @@ class Mirror {
  public:
     Mirror(uint16_t start, uint16_t end, uint16_t original_start)
         : start(start), end(end), original_start(original_start) {};
-    bool contains(uint16_t addr) { return (addr >= start) && (addr <= end); }
-    virtual uint16_t map(uint16_t addr) = 0;
+    bool contains(uint16_t addr);
+    uint16_t map(uint16_t addr);
 
  protected:
     uint16_t start, end, original_start;
