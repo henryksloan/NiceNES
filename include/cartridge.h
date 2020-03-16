cf4d8ae9658726f21c9a898eb89db70d85af39fe
Henry Sloan <henryksloan@gmail.com>
2020-03-16 10:22:42 -0400

    Implement population of a cartridge representation with flags and ROM

---
+++ include/cartridge.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <fstream>
+#include <vector>
+
+enum class MirroringMode {
+    HORIZONTAL,
+    VERTICAL,
+    FOURSCREEN,
+    SINGLESCREEN_LO,
+    SINGLESCREEN_HI
+};
+
+class Cartridge {
+ public:
+    Cartridge(std::ifstream &file)
+        : meta(populate_rom(file)) {};
+
+    const struct MetaData {
+        int n_prg_banks;
+        int n_chr_banks;
+        MirroringMode mirroring;
+        bool has_battery;
+        bool has_trainer;
+        uint8_t mapper;
+        bool is_VS;
+        bool is_PC10;
+    } meta;
+
+ private:
+    MetaData populate_rom(std::ifstream &file);
+
+    std::vector<uint8_t> prg; // Program ROM
+    std::vector<uint8_t> chr; // Character ROM
+
+    // Usually empty ROM sectors
+    std::vector<uint8_t> trainer; // Trainer ROM
+    std::vector<uint8_t> pc_irom; // PlayChoice INST-ROM
+    std::vector<uint8_t> pc_prom; // PlayChoice ROM
+};
